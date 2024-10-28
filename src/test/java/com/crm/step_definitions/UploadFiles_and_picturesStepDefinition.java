4ba76aeb3de94e0c2b903c96483f52a711714af9
Medeni Rostem <mrostem04@gmail.com>
2024-10-28 17:59:18 -0500

    I created upload step definition

---
+++ src/test/java/com/crm/step_definitions/UploadFiles_and_picturesStepDefinition.java
@@ -0,0 +1,28 @@
+package com.crm.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class UploadFiles_and_picturesStepDefinition {
+    @Given("I am on the Upload File page")
+    public void i_am_on_the_upload_file_page() {
+
+    }
+
+    @When("I upload a file named test.txt")
+    public void i_upload_a_file_named_test_txt() {
+
+    }
+
+    @When("I click the Upload button")
+    public void i_click_the_upload_button() {
+
+    }
+
+    @Then("I should see \"File uploaded successfully")
+    public void i_should_see_file_uploaded_successfully() {
+
+
+    }
+}
