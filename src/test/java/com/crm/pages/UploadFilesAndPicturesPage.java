cfd02c2dd6427dbe2a9359540c26cebaa679d23d
Medeni Rostem <mrostem04@gmail.com>
2024-10-31 14:05:27 -0500

    I updated that file too

--- src/test/java/com/crm/pages/UploadFilesAndPicturesPage.java
+++ src/test/java/com/crm/pages/UploadFilesAndPicturesPage.java
@@ -6,8 +6,18 @@ import org.openqa.selenium.support.FindBy;
 
 public class UploadFilesAndPicturesPage extends BasePage{
 
+
     @FindBy(xpath="//span[@id='bx-b-uploadfile-blogPostForm']")
     public  WebElement uploadFilesAndPicturesButton;
+
+    // Other elements and methods for uploading files and pictures go here...
+    String projectPath = System.getProperty("C:\\Users\\meden\\IdeaProjects\\B36_GR6-CRM_Sprint-2\\src\\test\\resources\\files");
+
+    //the path of the file
+    String filePathPNG = "C:\\Users\\meden\\IdeaProjects\\B36_GR6-CRM_Sprint-2\\src\\test\\resources\\files";
+
+    //it concatenates the project path and the file path to obtain the full path of the PNG file
+    //String fullPathJPG = projectPath + "C:\\Users\\meden\\IdeaProjects\\B36_GR6-CRM_Sprint-2\\src\\test\\resources\\files" + filePathPNG;
 
     public void upload(){
         uploadFilesAndPicturesButton.click();
